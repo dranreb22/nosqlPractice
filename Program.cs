637e5ab6807e652d7a3402ea458ac2d8d9c66844
berna <bernard2200@hotmail.com>
2021-02-09 21:23:16 -0500

    testing basic insert in LiteDB, C#, and noSQL

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using LiteDB;
+
+namespace nosqlPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new LiteDatabase(@"MyData.db")){
+                var users = db.GetCollection<User>("users");
+
+                var user = new User{
+                    Name = "Bernard Georges",
+                    Email = "testemail@hotmail.com"
+                };
+
+                users.Insert(user);
+
+                var result = users.Find(x => x.Email == "testemail@hotmail.com").FirstOrDefault();
+                Console.WriteLine(result.Name);
+
+                user.Name = "Bernard";
+                users.Update(user);
+
+                users.EnsureIndex( x => x.Name);
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,9 @@
+namespace nosqlPractice
+{
+    public class User{
+    public int ID {get; set;}
+    public string Name { get; set;}
+    public string Email { get; set;}
+ }
+
+}
